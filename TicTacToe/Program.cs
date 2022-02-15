94a743357641e6cab27506c5abe0043abc24247c
Lane Harris <lrharris215@gmail.com>
2022-02-15 14:14:16 -0500

    Add readme outline

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -6,7 +6,7 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Tic Tac Toe!");
         }
     }
 }
