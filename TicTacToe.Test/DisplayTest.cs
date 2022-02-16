67f3f0dd28ab761a563a6d0ef0b51ae5c9ed8af0
Lane Harris <lrharris215@gmail.com>
2022-02-16 15:50:29 -0500

    Add Display and Display Test

---
+++ TicTacToe.Test/DisplayTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using TicTacToe;
+using Xunit;
+
+namespace TicTacToe.Test
+{
+    public class DisplayTest
+    {
+        [Fact]
+        public void Print_Gives_Input_to_Output_Function()
+        { 
+            
+            var output = new StringWriter();
+            Console.SetOut(output);
+
+            string testString = "Testtesttest";
+
+            Display.Print(testString);
+           
+
+            Assert.Equal(output.ToString(), testString);
+
+        }
+    }
+}
---
+++ TicTacToe/Display.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace TicTacToe
+{
+    public class Display
+    {
+
+        public static void Print(string input)
+        {
+            Console.Write(input);
+        }
+    }
+}
