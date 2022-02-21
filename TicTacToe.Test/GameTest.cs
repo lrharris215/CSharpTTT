5d65f0abbb29a5a78f9871458f7fe002c7650a7f
Lane Harris <lrharris215@gmail.com>
2022-02-21 11:52:31 -0500

    Refactor: GameTest#TakeTurn is now a Theory test

--- TicTacToe.Test/GameTest.cs
+++ TicTacToe.Test/GameTest.cs
@@ -7,25 +7,31 @@ namespace TicTacToe.Test
 {
     public class GameTest
     {
-        [Fact]
-        public void TakeTurn_PlacesAValidMark_OnTheBoard()
+        [Theory]
+        [InlineData(3, 'M')]
+        [InlineData(4, 'Q')]
+        [InlineData(9, 'T')]
+        public void TakeTurn_PlacesAValidMark_OnTheBoard(int position, char mark)
         {
 
             Mock<Board> mockBoard = new Mock<Board>();
             Mock<Player> mockPlayer = new Mock<Player>();
-           
-            //Need to mock different answers: returns 3 for the validation, then returns M for formatting the board
-            mockBoard.SetupSequence(mb => mb.FindPosition(3)).Returns('3').Returns('M');
 
-            // MockPlayer has mark M
-            mockPlayer.Setup(mp => mp.Mark).Returns('M');
+            //For some reason Convert.ToChar() was breaking the test
+            char positionChar = position.ToString().ToCharArray()[0];
+
+            //Need to mock different answers: returns the <positionChar> for the validation, then returns <mark> for formatting the board
+            mockBoard.SetupSequence(mb => mb.FindPosition(position)).Returns(positionChar).Returns(mark);
+
+            // MockPlayer has the entered mark. 
+            mockPlayer.Setup(mp => mp.Mark).Returns(mark);
 
             // Setting Mock here to verify AddMark has been called
-            mockBoard.Setup(mb => mb.AddMark(3, 'M'));
+            mockBoard.Setup(mb => mb.AddMark(position, mark));
 
             Game game = new Game(mockBoard.Object, mockPlayer.Object, mockPlayer.Object);
 
-            var input = new StringReader("3");
+            var input = new StringReader(position.ToString());
             Console.SetIn(input);
 
             var output = new StringWriter();
@@ -33,8 +39,8 @@ namespace TicTacToe.Test
 
             game.TakeTurn(mockPlayer.Object);
 
-            //Verify's AddMark has been called once with these arguments
-            mockBoard.Verify(mb => mb.AddMark(3, 'M'), Times.Once());
+            //Verifies that AddMark has been called once with these arguments
+            mockBoard.Verify(mb => mb.AddMark(position, mark), Times.Once());
             
 
         }
